3a7e4f5dd7e95ef82b12dedca7c3b14aaf50ee7f
MatthewSmithseree <trulyegotistical@gmail.com>
2025-06-10 11:46:07 +0800

    Update data-ingestion.py
    Added fetch stock data function

--- data-ingestion.py
+++ data-ingestion.py
@@ -1,1 +1,15 @@
 # Moving Average Trading Bot - Data Ingestion Script
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+def fetch_stock_data(ticker, startdate, enddate):
+    dataframe = yf.download(ticker, start = startdate, end = enddate)
+    return dataframe
+
+testdata = fetch_stock_data("MSFT", "2023-01-01", "2024-12-31")
+print(testdata.head())
+
+
+
