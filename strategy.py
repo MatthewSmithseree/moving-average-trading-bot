b258f72f5f36d397d72b50575b760535754d7682
MatthewSmithseree <trulyegotistical@gmail.com>
2025-06-14 12:02:58 +0800

    Changed strategy
    Used 9, 21, and 55 SMA instead. Noticed a few errors, needs fixing.

--- strategy.py
+++ strategy.py
@@ -5,10 +5,40 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 
 def generate_signals(dataframe):
-    dataframe["SMA_50"] = dataframe['Close'].rolling(window=50).mean()
-    dataframe["SMA_200"] = dataframe['Close'].rolling(window=200).mean()
+    dataframe["SMA_9"] = dataframe['Close'].rolling(window=9).mean()
+    dataframe["SMA_21"] = dataframe['Close'].rolling(window=21).mean()
+    dataframe["SMA_55"] = dataframe['Close'].rolling(window=55).mean()
+
     dataframe["Signal"] = 0
-    dataframe["Signal"] = np.where(dataframe["SMA_50"] > dataframe["SMA_200"], 1, 
-                            np.where(dataframe["SMA_50"] < dataframe["SMA_200"], -1, 0))
-    dataframe.loc[(dataframe["SMA_50"].isna() | dataframe["SMA_200"].isna()), "Signal"] = 0
+
+    buy_signal = (
+        (
+            (dataframe["SMA_9"] > dataframe["SMA_21"]) | (dataframe["SMA_21"] > dataframe["SMA_55"])
+        )
+        & 
+        (dataframe["Close"] > dataframe["SMA_9"])
+    )
+    
+    sell_signal = (
+        (
+            (dataframe["SMA_9"] < dataframe["SMA_21"]) | (dataframe["SMA_21"] < dataframe["SMA_55"])
+        )
+        & 
+        (dataframe["Close"] < dataframe["SMA_9"])
+    )
+
+    dataframe.loc[buy_signal, "Signal"] = 1
+    dataframe.loc[sell_signal, "Signal"] = -1
+
+    dataframe.loc[dataframe["SMA_9"].isna(), "Signal"] = 0
+    dataframe.loc[dataframe["SMA_21"].isna(), "Signal"] = 0
+    dataframe.loc[dataframe["SMA_55"].isna(), "Signal"] = 0
+
     return dataframe
+
+# If (EMA(9) > EMA(21) OR EMA(21) > EMA(55)) AND Price > EMA(9):
+#     Buy
+# Elif (EMA(9) < EMA(21) OR EMA(21) < EMA(55)) AND Price < EMA(9):
+#     Sell
+# Else:
+#     Hold
