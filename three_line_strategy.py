2bd12a068ff42f5d2bc17d74efd9f94d5f928fe4
MatthewSmithseree <trulyegotistical@gmail.com>
2025-06-14 12:53:04 +0800

    Implemented 21/50/200
    Implemented 21/50/200 crossover trading strategy

--- three_line_strategy.py
+++ three_line_strategy.py
@@ -5,34 +5,34 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 
 def generate_signals(dataframe):
-    dataframe["SMA_9"] = dataframe['Close'].rolling(window=9).mean()
     dataframe["SMA_21"] = dataframe['Close'].rolling(window=21).mean()
-    dataframe["SMA_55"] = dataframe['Close'].rolling(window=55).mean()
+    dataframe["SMA_50"] = dataframe['Close'].rolling(window=50).mean()
+    dataframe["SMA_200"] = dataframe['Close'].rolling(window=200).mean()
 
     dataframe["Signal"] = 0
 
     buy_signal = (
         (
-            (dataframe["SMA_9"] > dataframe["SMA_21"]) & (dataframe["SMA_21"] > dataframe["SMA_55"])
+            (dataframe["SMA_21"] > dataframe["SMA_50"]) & (dataframe["SMA_50"] > dataframe["SMA_200"])
         )
         & 
-        (dataframe["Close"] > dataframe["SMA_9"])
+        (dataframe["Close"] > dataframe["SMA_21"])
     )
     
     sell_signal = (
         (
-            (dataframe["SMA_9"] < dataframe["SMA_21"]) & (dataframe["SMA_21"] < dataframe["SMA_55"])
+            (dataframe["SMA_21"] < dataframe["SMA_50"]) & (dataframe["SMA_50"] < dataframe["SMA_200"])
         )
         & 
-        (dataframe["Close"] < dataframe["SMA_9"])
+        (dataframe["Close"] < dataframe["SMA_21"])
     )
 
     dataframe.loc[buy_signal, "Signal"] = 1
     dataframe.loc[sell_signal, "Signal"] = -1
 
-    dataframe.loc[dataframe["SMA_9"].isna(), "Signal"] = 0
     dataframe.loc[dataframe["SMA_21"].isna(), "Signal"] = 0
-    dataframe.loc[dataframe["SMA_55"].isna(), "Signal"] = 0
+    dataframe.loc[dataframe["SMA_50"].isna(), "Signal"] = 0
+    dataframe.loc[dataframe["SMA_200"].isna(), "Signal"] = 0
 
     return dataframe
 
