90aa2715994ebee7b679b8b92aedc41185027e4f
MatthewSmithseree <trulyegotistical@gmail.com>
2025-06-19 11:55:23 +0800

    Added sharpe ratio + volatility
    Added sharpe ratio + volatility and plotted it on the graph by pinning it on the screen.

--- backtesting.py
+++ backtesting.py
@@ -10,6 +10,12 @@ def backtest_strategy(dataframe):
     dataframe["Strategy"] = dataframe["Return"] * dataframe["Position"]
     dataframe["Cumulative_Strategy"] = (1 + dataframe["Strategy"]).cumprod()
     dataframe["Cumulative_Return"] = (1 + dataframe["Return"]).cumprod()
+
+    avg_risk_free = 0.04396 / 252
+    daily_volatility = dataframe["Strategy"].std()
+    avg_return = dataframe["Strategy"].mean()
+    sharpe_ratio_annualized = np.sqrt(252) * ((avg_return - avg_risk_free)/daily_volatility)
+    
     # plotting
     plt.figure(figsize = (12, 6))
     plt.plot(dataframe["Cumulative_Return"], label = "Buy & Hold")
@@ -17,6 +23,22 @@ def backtest_strategy(dataframe):
     plt.title("Cumulative Performance")
     plt.ylabel("Portfolio Value %")
     plt.xlabel("Time")
+    plt.text(
+        0.90,                # X position (from 0 to 1)
+        0.95,                # Y position (from 0 to 1)
+        f'Sharpe: {sharpe_ratio_annualized:.2f}',
+        ha='right',          # horizontal alignment
+        transform=plt.gca().transAxes,
+        fontsize=10
+    )
+    plt.text(
+        0.90,
+        0.90,
+        f'Std: {daily_volatility:.5f}',
+        ha='right',
+        transform=plt.gca().transAxes,
+        fontsize=10
+    )
     plt.legend()
     plt.show()
     return dataframe
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from data_ingestion import fetch_stock_data
-from three_line_strategy import generate_signals
+from two_line_strategy import generate_signals
 from backtesting import backtest_strategy
 
 
-msftdata = fetch_stock_data("MSFT", "2018-01-01", "2024-12-31")
-msftdata = generate_signals(msftdata)
-msftdata = backtest_strategy(msftdata)
+data = fetch_stock_data("AAPL", "2018-01-01", "2024-12-31")
+data = generate_signals(data)
+data = backtest_strategy(data)
